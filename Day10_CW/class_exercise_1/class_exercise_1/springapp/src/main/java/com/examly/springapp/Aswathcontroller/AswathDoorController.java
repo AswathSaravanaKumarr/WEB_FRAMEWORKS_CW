61a6e8e76d369578a59ce192836369e6681f873c
AswathSaravanaKumarr <151117687+AswathSaravanaKumarr@users.noreply.github.com>
2024-04-24 12:43:50 +0530

    Add files via upload

---
+++ Day10_CW/class_exercise_1/class_exercise_1/springapp/src/main/java/com/examly/springapp/Aswathcontroller/AswathDoorController.java
@@ -0,0 +1,102 @@
+package com.examly.springapp.Aswathcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.Aswathmodel.AswathDoor;
+import com.examly.springapp.Aswathservice.AswathDoorService;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api")
+public class AswathDoorController {
+
+    @Autowired
+    public AswathDoorService doorService;
+
+    @PostMapping("/door")
+    public ResponseEntity<AswathDoor> postData(@RequestBody AswathDoor door) {
+        AswathDoor doorData = doorService.postData(door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("door/bycolor/{color}")
+    public ResponseEntity<List<AswathDoor>> getBycolor(@PathVariable String color) {
+        List<AswathDoor> door = doorService.getBycolor(color);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("door/{doorid}")
+    public ResponseEntity<AswathDoor> updateDetailResponseEntity(@PathVariable int doorid, @RequestBody AswathDoor door) {
+        AswathDoor doorData = doorService.updateDetail(doorid, door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("door/{doorid}")
+    public ResponseEntity<String> deleteDoor(@PathVariable int doorid) {
+        if (doorService.deleteDoor(doorid)) {
+            return new ResponseEntity<>("Door deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door/bydoortype/{doortype}")
+    public ResponseEntity<List<AswathDoor>> getByDoor(@PathVariable String doortype) {
+        List<AswathDoor> door = doorService.getByDoorType(doortype);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door")
+    public ResponseEntity<List<AswathDoor>> getAllDetails() {
+        List<AswathDoor> door = doorService.getAllDetails();
+        if (door != null) {
+
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    @GetMapping("/door/{doorid}")
+    public ResponseEntity<AswathDoor> getDetailsById(@PathVariable int doorid) {
+        AswathDoor door = doorService.getDetailsById(doorid);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
